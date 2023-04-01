549eef57872a9cb32f6eb7d675b31ab541c9b4b7
Nguyen Van Viet <Nvviet17.4@gmail.com>
2023-04-01 22:51:48 +0700

    Revert "Delete redundant file and add some folder to gitignore"
    This reverts commit 5984c85fd04117d7da1a24cbe8d54a32d18893f5.

---
+++ Phase_1/Form6.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Phase_1
+{
+    public partial class Form6 : Form
+    {
+        public Form6()
+        {
+            InitializeComponent();
+        }
+
+        private void Form6_Load(object sender, EventArgs e)
+        {
+            // Get the size of the primary screen
+            Rectangle workingArea = Screen.PrimaryScreen.WorkingArea;
+
+            // Calculate the location of the form to be centered on the screen
+            int x = (workingArea.Width - this.Width) / 2;
+            int y = (workingArea.Height - this.Height) / 2;
+
+            // Set the location of the form
+            this.Location = new Point(x, y);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form1 f1 = new Form1();
+            this.Hide();
+            f1.Show();
+        }
+    }
+}
